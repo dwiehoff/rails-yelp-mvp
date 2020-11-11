0d99195c42cc5b531c313a06736ce7a5d8bb9b85
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-11 12:04:30 +0100

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts '» Clearing of database ...'
+Restaurant.destroy_all
+
+puts '» Creating new entries ...'
+Restaurant.create!(name: 'PishPosh', address: 'London East Area', phone_number: '+49 1522 32423423423', category: 'french')
+Restaurant.create!(name: 'Ratskeller', address: 'Munich', phone_number: '+49 89 232322323', category: 'japanese')
+Restaurant.create!(name: 'Sim Sim', address: 'Munich', phone_number: '+49 89 45238928', category: 'belgian')
+Restaurant.create!(name: 'Chi Thu @ Fraunhofer', address: 'Munich', phone_number: nil, category: 'chinese')
+Restaurant.create!(name: "McDonald's", address: 'Munich', phone_number: nil, category: 'belgian')
+
+puts '» Finished seeding of database'
