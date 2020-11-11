21f3824ed207a301c8bd0751bd55fedbcde295ef
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-11 14:53:23 +0100

    Implemented reviews#new and #create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,9 +8,9 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    restaurant = Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.create(restaurant_params)
     if restaurant.save
-      redirect_to restaurants_path
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  # resources :restaurants, only: %i[index new create show]
-  # resources :reviews, only: %i[new create]
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[new create]
+  end
 
-  # user stories
-  # list all
-  get 'restaurants', to: 'restaurants#index'
-  # add new
-  get 'restaurants/new', to: 'restaurants#new'
-  post 'restaurants', to: 'restaurants#create'
-  # show single
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-  # add new review
-  get 'restaurants/:id/reviews/new', to: 'reviews#new'
-  post 'restaurants/:id/reviews', to: 'reviews#create'
+
+  # # user stories
+  # # list all
+  # get 'restaurants', to: 'restaurants#index'
+  # # add new
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # show single
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # # add new review
+  # get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :reviews_new
+  # post 'restaurants/:id/reviews', to: 'reviews#create'
 end
