7ba20abdba24b5d92abf6f25ac04739b6ac473c5
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-11 15:17:43 +0100

    Refactored and styled buttons

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,10 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
@@ -16,6 +15,10 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 
   def review_params
     params.require(:review).permit(:rating, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,4 @@ Rails.application.routes.draw do
   resources :restaurants, only: %i[index new create show] do
     resources :reviews, only: %i[new create]
   end
-
-
-  # # user stories
-  # # list all
-  # get 'restaurants', to: 'restaurants#index'
-  # # add new
-  # get 'restaurants/new', to: 'restaurants#new'
-  # post 'restaurants', to: 'restaurants#create'
-  # # show single
-  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-  # # add new review
-  # get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :reviews_new
-  # post 'restaurants/:id/reviews', to: 'reviews#create'
 end
